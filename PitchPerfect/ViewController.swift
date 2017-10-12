97e6d31a25c9941f1a96f60606e8c1821ef3e4bb
AhmadL <a24lakha@uwaterloo.ca>
2017-10-12 10:22:38 -0600

    Navigation controller added

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,10 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,11 +33,16 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(_ sender: Any) {
         print("The button was pressed")
         recordingLabel.text = "Recording in progress..."
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
     
     @IBAction func stopRecording(_ sender: Any) {
         print("stopped recording")
-        recordingLabel.text = "Recording stopped..."
+        recordingLabel.text = "Tap to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
+    
 }
 
