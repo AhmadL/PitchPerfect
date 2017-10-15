a6a956c8c01a6a9b4b4e8a6f1569e00eeb910da5
AhmadL <a24lakha@uwaterloo.ca>
2017-10-15 16:34:48 -0600

    Working version of app

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -25,7 +25,6 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("viewWillAppear called")
     }
 
     @IBAction func recordAudio(_ sender: Any) {
